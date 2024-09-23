e5581912c97521c6ba1770d29ae29dc8e2b3216c
tadosuke <tadosuke@gmail.com>
2024-09-23 15:38:36 +0900

    ファイルの上げ忘れ

---
+++ Assets/Scripts/WindUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WindUI : MonoBehaviour
+{
+    public VariablesSO variables;  // ゲーム内変数
+
+    private TextMeshProUGUI _text;  // 時間切れテキスト
+
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        _text.text = $"Wind: {variables.wind}";
+    }
+}
